e9ef9c47e28331d9491a4f59c08e9bad3a669bd5
Rida AMINE <rida.amine.1994@gmail.com>
2022-09-23 12:51:54 +0000

    Init

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+var currentConversion = 'CtoF';
+var current = 0;
+var conversions = ['CtoF', 'FtoC'];
+
+function switchField(){
+  if(currentConversion == 'CtoF')
+  {
+    $('#celsius').prop( "disabled", false);
+    $('#fahrenheit').prop( "disabled", true);
+  }
+  else
+  {
+    $('#celsius').prop( "disabled", true);
+    $('#fahrenheit').prop( "disabled", false);
+  }
+}
+
 $(document).ready(function() {
+  switchField();
   $('#convert').click(function(){
-    var cels = parseInt($('#celsius').val());
-    $('#fahrenheit').val((cels * 9.0/5.0) + 32.0);
+    if(currentConversion === 'CtoF')
+    {
+      var cels = parseInt($('#celsius').val());
+      $('#fahrenheit').val(Math.round((cels * 9.0/5.0) + 32.0, 2));
+    }
+    else
+    {
+      var fahr = parseInt($('#fahrenheit').val());
+      $('#celsius').val(Math.round((fahr - 32.0) * 5.0/9.0, 2));
+    }
   });    
+  $('#switcher').click(function(){
+    current = !current + 0;
+    currentConversion = conversions[current];
+    switchField();
+  });
 });
